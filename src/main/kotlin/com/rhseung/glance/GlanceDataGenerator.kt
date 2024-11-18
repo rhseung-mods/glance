4fe87f8973537f515b1ea495e6e617d497dc3be4
Hyunseung Ryu <rhseungg@gmail.com>
2024-11-18 19:43:00 +0900

    Initial commit

---
+++ src/main/kotlin/com/rhseung/glance/GlanceDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.rhseung.glance
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object GlanceDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
